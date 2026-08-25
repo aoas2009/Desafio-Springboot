bcbecf02669d1246d146f630237e3c6d2dab9400
aoas2009 <aoasilva122009@hotmail.com>
2026-08-25 20:51:08 -0300

    feat: criando entidade Funcionario

---
+++ src/main/java/org/example/desafiopicpay/core/entity/Funcionario.java
@@ -0,0 +1,47 @@
+package org.example.desafiopicpay.core.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.example.desafiopicpay.core.valueobject.Status;
+
+import java.util.UUID;
+
+@Getter
+public class Funcionario {
+    private UUID id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cargo;
+    private String departamento;
+    private double salario;
+    private String cidade;
+    private Status status;
+
+    public Funcionario(String nome, String email, String telefone, String cargo, String departamento, double salario, String cidade) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.cargo = cargo;
+        this.departamento = departamento;
+        this.salario = salario;
+        this.cidade = cidade;
+
+        this.id = UUID.randomUUID();
+        this.status = Status.EM_ANALISE;
+    }
+
+    public void negar() {
+        this.status = Status.REPROVADO;
+    }
+
+    public void aceitar() {
+        this.status = Status.APROVADO;
+    }
+
+    public void contratar() {
+        this.status = Status.CONTRATADO;
+    }
+
+
+}
---
+++ src/main/java/org/example/desafiopicpay/core/valueobject/Status.java
@@ -0,0 +1,8 @@
+package org.example.desafiopicpay.core.valueobject;
+
+public enum Status {
+    EM_ANALISE,
+    APROVADO,
+    REPROVADO,
+    CONTRATADO
+}
