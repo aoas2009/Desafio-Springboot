e068f27d52970af4cf861bc20c512e715f31e000
aoas2009 <aoasilva122009@hotmail.com>
2026-08-26 22:49:37 -0300

    fix: configurando cors para aceitar req do front part2?

--- frontend/js/todos-candidatos.js
+++ frontend/js/todos-candidatos.js
@@ -1,6 +1,6 @@
 async function buscarFuncionarios() {
   try {
-    const response = await fetch('http://localhost:8080/api/funcionario');
+    const response = await fetch('http://localhost:8080/api/funcionario/health');
     const data = await response.json();
     console.log(data);
   } catch (error) {
