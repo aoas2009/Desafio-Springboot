1921f98de1994a761b05f2e5fed66c16a2496f22
aoas2009 <aoasilva122009@hotmail.com>
2026-08-26 22:29:51 -0300

    test: testando chamada de api

---
+++ frontend/js/todos-candidatos.js
@@ -0,0 +1,11 @@
+async function buscarFuncionarios() {
+  try {
+    const response = await fetch('http://localhost:8080/api/funcionario');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+}
+
+buscarFuncionarios();
